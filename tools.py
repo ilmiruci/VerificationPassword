7b986f326bbda984e8cbc73871d19ca84c763aa3
ilmir <adm.eths@gmail.com>
2025-02-25 20:51:59 +0500

    update main.py and tools.py

--- tools.py
+++ tools.py
@@ -3,23 +3,55 @@ import string  # Удерживать CTRL + Left Click
 # TODO: Использование string
 POSSIBLE_CHARS: str = string.ascii_letters + string.digits + "_"
 
-print(POSSIBLE_CHARS)
 
-
-def check_password(text: str) -> None:
+def check_password(text: str) -> str | bool:
+    """
+    Проверка пароля
+    :param text: пароль пользователя
+    :return: Сообщение - Пароль принят! если пароль прошел проверку
+    True - Если пароль прошел проверку
+    False - Если пароль не прошел проверку
+    """
     errors: list[str] = []
 
-    # Пароль должен начинаться с заглавной буквы
+    # [Проверка пароля] Пароль должен начинаться с заглавной буквы
     if not text[0].isupper():
         errors.append("Пароль должен начинаться с заглавной буквы.")
 
-    # Пароль должен заканчиваться только латинской буквой или цифрой.
-
+    # [Проверка пароля] Пароль должен состоять только из латинских букв, цифр и
+    # символа нижнего подчёркивания (_)
     for char in text:
         if char not in POSSIBLE_CHARS:
-            print("Пароль должен состоять только из латинских букв, "
-                  "цифр и символа нижнего подчёркивания (_).")
+            errors.append("Пароль должен состоять только из латинских букв, "
+                          "цифр и символа нижнего подчёркивания (_).")
             break
 
-    for error in errors:
-        print(error)
+    # [Проверка пароля] Пароль должен заканчиваться только латинской буквой или цифрой.
+    if text[-1] not in string.ascii_letters + string.digits:
+        errors.append("Пароль должен заканчиваться только латинской буквой или цифрой.")
+
+    # [Проверка пароля] Минимальная длина пароля — 12 символов, максимальная — 32 символа.
+    if len(text) < 12 or len(text) > 32:
+        errors.append("Минимальная длина пароля — 12 символов, максимальная — 32 символа.")
+
+    # Вывод ошибок на экран
+    if errors:
+        print("Пароль несоответствует требованиям!")
+        for error in errors:
+            print(error)
+        return False
+    else:
+        print("Пароль принят!")
+        return True
+
+
+# Запись пароля в файл
+def write_password(user_password):
+    """
+    Запись валидного пароля в файл
+    :param user_password: Пользовательский пароль
+    :return: none
+    """
+    valid_passwords = open("valid_passwords.txt", "a")
+    valid_passwords.write(user_password + "\n")
+    valid_passwords.close()
